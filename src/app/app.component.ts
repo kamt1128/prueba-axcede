dd9604e5de6e92a12d4ecc3888035102446e3f80
Kenny Morelo <kamt1128@gmail.com>
2024-05-29 16:27:38 -0500

    Home design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'front-axede';
+  accommodationTypes = [
+    'Estándar',
+    'Premium',
+    'VIP'
+  ]
 }
